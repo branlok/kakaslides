52d120d8ab86fcdf22cfa2024e76aad61740c10a
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2023-10-18 17:06:46 -0400

    build containers for text

--- src/Components/SlideContainer/index.tsx
+++ src/Components/SlideContainer/index.tsx
@@ -1,17 +1,13 @@
-import { useEffect, useLayoutEffect, useRef } from "react"
+import { useLayoutEffect, useRef } from "react"
 import useBlackbarSettings from "../../store/slices/slideModifications"
 import "./styled.css"
-import TextBox from "./TextBox";
-import { SettingWrapper } from "./TextBox/TextSetting";
 import BgImage from "./BgImage";
-import useContent from "../../store/slices/content";
 import TextContainer from "./TextContainer";
 
 type Props = {}
 
 function SlideContainer({ }: Props) {
     let blackBarsVisual = useBlackbarSettings(state => state.blackBarsVisual);
-    let [TITLE, SUBTITLE, FOOTERTEXT] = useContent(state => state.presets);
     let mySlide = useRef<HTMLElement>(null);
     useLayoutEffect(() => {
         console.log(mySlide.current);
@@ -28,19 +24,7 @@ function SlideContainer({ }: Props) {
             <TextContainer defaultText="赤 齣" defaultSize={"md"} />
             <TextContainer defaultText="Aka." />
             <TextContainer defaultText="(135)" defaultSize="xxs" />
-            {/* <div className="title-subtitle-wrapper">
-                <SettingWrapper settingsFor={TITLE}>
-                    <TextBox type={TITLE} />
-                </SettingWrapper>
-                <SettingWrapper settingsFor={SUBTITLE}>
-                    <TextBox type={SUBTITLE} />
-                </SettingWrapper>
-            </div>
-            <div className="footer-title-wrapper">
-                <SettingWrapper settingsFor={FOOTERTEXT}>
-                    <TextBox type={FOOTERTEXT} />
-                </SettingWrapper>
-            </div> */}
+
         </div>
     )
 }
